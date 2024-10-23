460228619fa597de90b6f413d41797064724d4f6
mhishmeh <hishmoe@gmail.com>
2024-10-23 18:03:01 -0500

    added assets page (logo)

--- main.go
+++ main.go
@@ -7,16 +7,19 @@ import (
 
 func createServer() {
 	mux := http.NewServeMux()
-	server := http.Server{
+	server := &http.Server{
 		Handler: mux,
 		Addr:    ":8080",
 	}
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/assets", http.FileServer(http.Dir("./assets/logo.png")))
 	addr := server.Addr
 	// Start the server
 	fmt.Println("Starting server on", addr)
 	if err := server.ListenAndServe(); err != nil {
 		fmt.Println("Error starting server:", err)
 	}
+
 }
 
 func main() {
